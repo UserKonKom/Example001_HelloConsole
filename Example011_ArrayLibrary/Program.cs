fb5d195a0b2d7c55777ad524ebba488e78e90d88
Konstantin <lolkup705@gmail.com>
2023-08-04 15:16:21 +0900

    add Example 011 Array Library

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -21,7 +21,28 @@ void PrintArray(int[] col)
     }
 }
 
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 4);
+Console.WriteLine(pos);
